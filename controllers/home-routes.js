d1d80edf2844fa330f743da7f4beecc6c9c76256
Thuy Nguyen <ttruong06@gmail.com>
2022-05-19 12:49:40 -0400

    updated code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require ('../models');
 
 router.get('/', (req, res) => {
+    console.log('======================');
     Post.findAll({
       attributes: [
         'id',
@@ -27,10 +28,8 @@ router.get('/', (req, res) => {
       ]
     })
       .then(dbPostData => {
-        console.log(dbPostData[0]);
-
-        const posts = dbPostData.map(post => post.get({ plain: true }))
-        // pass a single post object into the homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+  
         res.render('homepage', { posts });
       })
       .catch(err => {
